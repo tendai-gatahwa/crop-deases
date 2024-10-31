961a3e8b47e5f0444418ae7eaae825d8504a0322
tendai-gatahwa <107326659+tendai-gatahwa@users.noreply.github.com>
2024-10-31 07:19:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
 supplement_info = pd.read_csv('supplement_info.csv',encoding='cp1252')
 
 model = CNN.CNN(39)    
-model.load_state_dict(torch.load("plant_disease_model_1_latest.pt", weights_only=True))
+model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
 
 model.eval()
 
